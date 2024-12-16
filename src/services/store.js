a7758c9523f8f11d28336200ef869e9c7b82e5d8
Olga <olgaduk@mail.ru>
2024-12-16 15:44:51 +0600

    feature add user to redux and request functions

--- src/services/store.js
+++ src/services/store.js
@@ -4,12 +4,14 @@ import {constructorReducer} from './reducers/constructor'
 import {orderReducer} from './reducers/order'
 import {ingredientsReducer} from './reducers/ingredients'
 import {detailsReducer} from './reducers/details'
+import {userReducer} from './reducers/user'
 
 const rootReducer = combineReducers({
     burgerConstructor: constructorReducer,
     details: detailsReducer,
     ingredients: ingredientsReducer,
-    order: orderReducer
+    order: orderReducer,
+    user: userReducer
 })
 
 const initialState = {}
