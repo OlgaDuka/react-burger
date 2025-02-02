dad438f1b9470cf647a94e4d404c63d42cb14555
Olga <olgaduk@mail.ru>
2025-02-02 14:50:53 +0600

    feat add ws to profile page Orders

--- src/pages/profile-page/orders.tsx
+++ src/pages/profile-page/orders.tsx
@@ -1,7 +1,23 @@
-import React, {FC} from 'react'
+import React, {FC, useEffect} from 'react'
 import FeedList from "../../components/feed/feed-list/feed-list";
+import {useAppDispatch} from "../../services";
+import {wsConnectionClosed, wsConnectionStart} from "../../services/slices/ws";
+import {STORAGE_KEY, WS_URL_USER} from "../../utils/constants";
 
 const Orders: FC = () => {
+  const dispatch = useAppDispatch()
+  const token = localStorage.getItem(STORAGE_KEY.ACCESS)
+  const ws_str_init = WS_URL_USER + `?token=${token}`
+
+  useEffect(() => {
+    dispatch(wsConnectionStart(ws_str_init))
+
+    return () => {
+      dispatch(wsConnectionClosed())
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div>
       <FeedList />
